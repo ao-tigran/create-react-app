3bc5247d0aa69b60652ccfd66334319164dda22b
Mushegh Yeghiazaryan <mushegh@aobyte.com>
2019-09-16 11:48:43 +0400

    Define simple routes and introdue corresponding components

---
+++ src/components/AboutScreen/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./index.module.scss";
+
+const AboutScreen = props => {
+  return (
+    <div id={styles.login_screen}>
+      For authenticated and unauthenticated users
+    </div>
+  );
+};
+
+export default AboutScreen;
