83032444de975f5ded45ca2f7e8b386b8dd1d64d
Mushegh Yeghiazaryan <mushegh@aobyte.com>
2019-09-20 16:23:04 +0400

    Simple login/logout logic with Context API

--- src/containers/AuthenticationRoute/index.js
+++ src/containers/AuthenticationRoute/index.js
@@ -1,26 +1,54 @@
 import React from "react";
 import { Route, Redirect, withRouter } from "react-router-dom";
+import { AuthContext } from "./../../hooks/useAuth";
 
 class AuthenticationRoute extends Route {
-  isValidRouting = () => {
-    const { withAuth, isAuthenticated } = this.props;
+  isValidRouting = isAuthed => {
+    /** withAuth can take false/true/undefined
+     * false - means that you can enter the route only if you are not authenticated
+     * true - means that you can enter the route only if you are authenticated
+     * undefined - create auth insensitive route, meaning that you can enter it in any case
+     */
+    const { withAuth } = this.props;
 
-    // withAuth can take false/true/undefined
-    // false - means that you can enter the route only if you are not authenticated
-    // true - means that you can enter the route only if you are authenticated
-    // undefined - create auth insensitive route, measning that you can enter it in any case
     if (withAuth === undefined) {
       return true;
     }
 
-    return withAuth ? isAuthenticated : !isAuthenticated;
+    return withAuth ? isAuthed : !isAuthed;
   };
 
+  resolveRedirectPath = () => {
+    const { redirectOnFailure, location } = this.props;
+
+    if (redirectOnFailure) {
+      return redirectOnFailure;
+    }
+
+    // Redirect to the route, the user tried to access
+    // right before authentication
+    return location.state ? location.state.from : "/home";
+  }
+
   render() {
-    return this.isValidRouting() ? (
-      super.render()
-    ) : (
-      <Redirect to={this.props.redirectOnFailure} />
+    const { location } = this.props;
+    const redirectOnFailure = this.resolveRedirectPath();
+
+    return (
+      <AuthContext.Consumer>
+        {({ isAuthed }) => {
+          return this.isValidRouting(isAuthed) ? (
+            super.render()
+          ) : (
+            <Redirect
+              to={{
+                pathname: redirectOnFailure,
+                state: { from: location.pathname }
+              }}
+            />
+          );
+        }}
+      </AuthContext.Consumer>
     );
   }
 }
