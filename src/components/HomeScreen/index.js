d2abfd03f9e44da4e3be495a9d357aa7b5278b24
Manan <manana@aobyte.com>
2019-10-01 18:36:22 +0400

    Semantic UI React integrated

--- src/components/HomeScreen/index.js
+++ src/components/HomeScreen/index.js
@@ -3,6 +3,7 @@ import { useTranslation } from "react-i18next";
 import LanguageContainer from "./../LanguageContainer";
 import DigitInput from "./../inputs/DigitInput";
 import styles from "./index.module.scss";
+import { Header } from "semantic-ui-react";
 
 const HomeScreen = props => {
   const { t } = useTranslation();
@@ -11,6 +12,7 @@ const HomeScreen = props => {
   return (
     <div id={styles.home_screen}>
       <h1> {t("home.title")}</h1>
+      <Header as="h3" textAlign="center" content="Semantic UI React integrated"/>
       <LanguageContainer />
 
       <DigitInput
